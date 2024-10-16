4b9572fb1a132e32ace4cc72ccbd07ff0cefeace
BharatV <bharat.venkatesh.bv@gmamil.com>
2024-10-16 13:42:36 -0400

    Added creation and printing of table

--- Main.java
+++ Main.java
@@ -1,3 +1,22 @@
+import java.util.*;
+import java.math.*;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        int x = 5;
+        int[][] table = new int[9][9];
+        for(int i=0; i<table.length; i++) {
+            for(int j=0; j<table[i].length; j++) {
+                table[i][j] = (int)(9*Math.random())+1;
+                System.out.print(table[i][j]);
+                if((j+1)%3==0) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+            if((i+1)%3==0) {
+                System.out.println();
+            }
+        }
+    }
 }
