1ed0d8c75399206e946a46e663e016b3b8b367e9
BharatV <bharat.venkatesh.bv@gmamil.com>
2024-10-16 14:11:39 -0400

    Added input and output of table

--- Main.java
+++ Main.java
@@ -1,22 +1,46 @@
 import java.util.*;
 import java.math.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 5;
         int[][] table = new int[9][9];
-        for(int i=0; i<table.length; i++) {
-            for(int j=0; j<table[i].length; j++) {
-                table[i][j] = (int)(9*Math.random())+1;
-                System.out.print(table[i][j]);
-                if((j+1)%3==0) {
-                    System.out.print(" ");
+        try{
+            //tries to find a file from the input, appends .txt
+            File fil = new File(args[0]+".txt");
+            Scanner fileReader = new Scanner(fil);
+
+            //puts input from the file into the table, input must have spaces after every value and a new line when its a new row of the sudoku puzzle
+            for(int i=0; i<table.length; i++) {
+                for(int j=0; j<table[i].length; j++) {
+                    table[i][j] = fileReader.nextInt();
+                }
+                if(fileReader.hasNextLine()) {
+                    fileReader.nextLine();
                 }
             }
-            System.out.println();
-            if((i+1)%3==0) {
+
+            //prints out the table
+            for(int i=0; i<table.length; i++) {
+                for(int j=0; j<table[i].length; j++) {
+                System.out.print(table[i][j]+" ");
+                    if((j+1)%3==0 && j<7) {
+                        System.out.print("|");
+                    }
+                }
                 System.out.println();
+                if((i+1)%3==0 && i<7) {
+                    System.out.println("-------------------");
+                }
             }
         }
+        catch (FileNotFoundException e){
+            System.out.println("File not found ERROR");
+            e.printStackTrace();
+            return;
+        }
+
+        
+        
     }
 }
