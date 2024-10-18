ebde3b9d2860c8a5a66898824c879a50300dfa63
mskw <bharat.venkatesh.bv@gmamil.com>
2024-10-17 21:56:54 -0400

    outputs a solved grid if there is one

--- Main.java
+++ Main.java
@@ -2,68 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        int[][] table = new int[9][9];
-        int[][][] notes = new int[9][9][9];
-        try{
-            //tries to find a file from the input, appends .txt
-            File fil = new File(args[0]);
-            Scanner fileReader = new Scanner(fil);
+    public static int[][] table = new int[9][9];
+    public static int[][][] notes = new int[9][9][9];
 
-            //puts input from the file into the table, input must have spaces after every value and a new line when its a new row of the sudoku puzzle
-            for(int i=0; i<table.length; i++) {
-                for(int j=0; j<table[i].length; j++) {
-                    table[i][j] = fileReader.nextInt();
-
-                    //filling in notes array with null or possible values for the entry
-                    if(table[i][j] == 0) {
-                        for(int k=0; k<notes[i][j].length; k++) {
-                            notes[i][j][k] = k+1;
-                        }
-                    }
-                    else {
-                        notes[i][j] = null;
-                    }
-                }
-                if(fileReader.hasNextLine()) {
-                    fileReader.nextLine();
+    public static void printTable() {
+        for(int i=0; i<table.length; i++) {
+            for(int j=0; j<table[i].length; j++) {
+            System.out.print(table[i][j]+" ");
+                if((j+1)%3==0 && j<7) {
+                    System.out.print("| ");
                 }
             }
-
-            //prints out the table
-            for(int i=0; i<table.length; i++) {
-                for(int j=0; j<table[i].length; j++) {
-                System.out.print(table[i][j]+" ");
-                    if((j+1)%3==0 && j<7) {
-                        System.out.print("| ");
-                    }
-                }
-                System.out.println();
-                if((i+1)%3==0 && i<7) {
-                    System.out.println("---------------------");
-                }
+            System.out.println();
+            if((i+1)%3==0 && i<7) {
+                System.out.println("---------------------");
             }
-            fileReader.close();
         }
-        //Returns error if file not found
-        catch (FileNotFoundException e){
-            System.out.println("File not found ERROR");
-            e.printStackTrace();
-            return;
-        }
+    }
 
-
-        //Goes through columns and rows and if there is a non zero value adds it to a arraylist
+    //Goes through rows and if there is a non zero value, removes it from notes
+    public static void checkRows() {
         for(int i=0; i<table.length; i++) {
             ArrayList<Integer> entriesR = new ArrayList<Integer>();
-            ArrayList<Integer> entriesC = new ArrayList<Integer>();
             for(int j=0; j<table[i].length; j++) {
                 if(table[i][j] != 0) {
                     entriesR.add(table[i][j]);
-                }
-
-                if(table[j][i] != 0) {
-                    entriesC.add(table[j][i]);
                 }
             }
 
@@ -78,8 +41,21 @@ public class Main {
                         }
                     }
                 }
+            }
+        }
+    }
+    
+    //Goes through columns and if there is a non zero value, removes it from notes
+    public static void checkColumns() {
+        for(int i=0; i<table.length; i++) {
+            ArrayList<Integer> entriesC = new ArrayList<Integer>();
+            for(int j=0; j<table[i].length; j++) {
+                if(table[j][i] != 0) {
+                    entriesC.add(table[j][i]);
+                }
+            }
 
-
+            for(int j=0; j<notes[i].length; j++) {
                 for(int p=0; p<entriesC.size(); p++) {
                     //if a value in entries is in the notes COLUMNS then it is removed
                     if(notes[j][i] != null) {
@@ -92,8 +68,10 @@ public class Main {
                 }
             }
         }
-        
-        //creates subtables for each 3x3 grid
+    }
+
+    //creates subtables for each 3x3 grid
+    public static int[][] createSubTables() {
         int[][] subtables = new int[9][9];
         for(int i=0; i<table.length; i++) {
             for(int j=0; j<notes[i].length; j++) {
@@ -108,7 +86,10 @@ public class Main {
                 else if(i < 9 && j < 9) {subtables[8][j-6+(3*(i%3))] = table[i][j];}
             }
         }
+        return subtables;
+    }
 
+    public static void checkGrid(int[][] subtables) {
         ArrayList<ArrayList<Integer>> entriesG = new ArrayList<ArrayList<Integer>>();
         for(int i=0; i<subtables.length; i++) {
             entriesG.add(new ArrayList<Integer>());
@@ -140,9 +121,90 @@ public class Main {
                 }
             }
         }
-        
+    }
+
+    //goes through the notes array and checks if there is any single non zero element, if so then enters it into table
+    public static void restruct() {
+        for(int i=0; i<notes.length; i++) {
+            for(int j=0; j<notes[i].length; j++) {
+                int index=-1;
+                if(notes[i][j] != null) {
+                    for(int k=0; k<notes[i][j].length; k++) {
+                        if(notes[i][j][k] != 0) {
+                            if(index == -1) {
+                                index = k;
+                            }
+                            else if(index != -1) {
+                                index = -1;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if(index != -1) {
+                    table[i][j] = notes[i][j][index];
+                }
+            }
+        }
+    }
+
+    public static boolean isComplete() {
+        for(int i=0; i<table.length; i++) {
+            for(int j=0; j<table[i].length; j++) {
+                if(table[i][j] == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int[][] subtables = null;
+        try{
+            //tries to find a file from the input, appends .txt
+            File fil = new File(args[0]);
+            Scanner fileReader = new Scanner(fil);
+
+            //puts input from the file into the table, input must have spaces after every value and a new line when its a new row of the sudoku puzzle
+            for(int i=0; i<table.length; i++) {
+                for(int j=0; j<table[i].length; j++) {
+                    table[i][j] = fileReader.nextInt();
+
+                    //filling in notes array with null or possible values for the entry
+                    if(table[i][j] == 0) {
+                        for(int k=0; k<notes[i][j].length; k++) {
+                            notes[i][j][k] = k+1;
+                        }
+                    }
+                    else {
+                        notes[i][j] = null;
+                    }
+                }
+                if(fileReader.hasNextLine()) {
+                    fileReader.nextLine();
+                }
+            }
+
+            printTable();
+            fileReader.close();
+        }
+        //Returns error if file not found
+        catch (FileNotFoundException e){
+            System.out.println("File not found ERROR");
+            e.printStackTrace();
+            return;
+        }
+
+        while(!isComplete()) {
+            checkRows();
+            checkColumns();
+            subtables = createSubTables();
+            checkGrid(subtables);
+            restruct();
+        }
 
 
+            //--------------------------------------------only--printing--below-----------------------------------------------------------------------------
 
         
         //prints out the subtables
@@ -154,6 +216,8 @@ public class Main {
             }
             System.out.println("\n");
         }
+
+
 
         System.out.println();
         //prints out the row notes
@@ -176,7 +240,10 @@ public class Main {
             System.out.println();
         }
         
+        System.out.println();
 
+        //prints out the table
+        printTable();
         
     }
 }
